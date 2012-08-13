7887e376dd70b1f42c85b90c3dd4db986a3de0a9
tselliach <thiago.selliach@gmail.com>
2012-08-13 20:34:02 -0300

    Exercicio da aula 3 de laboratorio

---
+++ aula3/src/main/java/br/com/tpselliach/aula3/App.java
@@ -0,0 +1,13 @@
+package br.com.tpselliach.aula3; //<--- Este caminho é chamada de NameSpace
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
