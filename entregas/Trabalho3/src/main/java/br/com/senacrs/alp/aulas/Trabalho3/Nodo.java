87c9d8049a5ef49993fbb9e7e019fbcc48e4f150
tselliach <thiago.selliach@gmail.com>
2012-08-30 19:00:37 -0300

    renomear projeto

---
+++ entregas/Trabalho3/src/main/java/br/com/senacrs/alp/aulas/Trabalho3/Nodo.java
@@ -0,0 +1,24 @@
+package br.com.senacrs.alp.aulas.Trabalho3;
+
+public class Nodo<Tipo extends Object> {
+	private Tipo valor;
+	private Nodo<Tipo> proximo;
+	public Nodo(Tipo valor) {
+		
+	}
+	public Tipo getValor() {
+		return valor;
+	}
+	public void setValor(Tipo valor) {
+		this.valor = valor;
+	}
+	public Nodo<Tipo> getProximo() {
+		return proximo;
+	}
+	public void setProximo(Nodo<Tipo> proximo) {
+		this.proximo = proximo;
+	}
+	
+	
+
+}
