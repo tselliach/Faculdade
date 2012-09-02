4c8068ab32b32e3c21e8cc037793de3c029aaf25
Thiago <thiago.selliach@gmail.com>
2012-09-02 19:00:16 -0300

    2º commit

---
+++ entregas/Trabalho4/src/main/java/br/com/senacrs/alp/aulas/Trabalho4/Main.java
@@ -0,0 +1,14 @@
+package br.com.senacrs.alp.aulas.Trabalho4;
+
+public class Main {
+	public static void main(String[] args) {
+		MinhaLista<String> lista = new MinhaListaImp<String>();
+		MinhaLista<Integer> lista2 = new MinhaListaImp<Integer>();
+		lista.prefixar("novoInicio");
+		lista.sufixar("novoFim");
+		System.out.println(lista.tamanho());
+		System.out.println(lista.buscar(0));
+
+		System.out.println(lista.tamanho());
+	}
+}
---
+++ entregas/Trabalho4/src/main/java/br/com/senacrs/alp/aulas/Trabalho4/MinhaLista.java
@@ -0,0 +1,11 @@
+package br.com.senacrs.alp.aulas.Trabalho4;
+
+public interface MinhaLista<Tipo extends Object> {
+
+		void sufixar(Tipo valor);
+		void prefixar(Tipo valor);
+		Tipo buscar(int posicao);
+		void inserir(int posicao, Tipo valor);
+		Tipo remover(int posicao);
+		int tamanho();
+}
