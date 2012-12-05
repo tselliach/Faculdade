16c8c5a1f87418d39f6effbc414e507a1eb5bb9a
Thiago Selliach <thiago.selliach@gmail.com>
2012-12-04 22:05:28 -0200

    entrega trabalho 13

---
+++ exercicios/comparator/src/Main.java
@@ -0,0 +1,9 @@
+package br.com.senacrs.alp.aulas.comparator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+	List<Funcionario> lista = new LinkedList<Funcionario>();
+}
---
+++ exercicios/comparator/src/main/java/br/com/senacrs/alp/aulas/comparator/FuncionarioComparator.java
@@ -0,0 +1,6 @@
+package br.com.senacrs.alp.aulas.comparator;
+
+public interface FuncionarioComparator {
+	int compare(Funcionario nome, Funcionario salario);
+
+}
